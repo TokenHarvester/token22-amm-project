2504f18ea6292a22c73a172e82048e4dd0584de0
ElochukwuOrji <orjielochukwu3@gmail.com>
2025-08-10 11:02:34 +0100

    deleted old ui

--- ui/src/utils/constants.ts
+++
@@ -1,8 +0,0 @@
-import { PublicKey } from '@solana/web3.js';
-
-export const TOKEN_2022_PROGRAM_ID = new PublicKey('TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb');
-export const ASSOCIATED_TOKEN_PROGRAM_ID = new PublicKey('ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL');
-
-export const SWAP_PROGRAM_ID = new PublicKey('H8j9y1sARxb73rL2RFqfpKG8Yi9CYXmgcha5n7QLYmbU'); 
-export const HOOK_PROGRAM_ID = new PublicKey('EYzLSaUydn5GLBov2G1ypmL7s3zYqNyNfxY3BQZ7Rdy2'); 
-export const DEVNET_ENDPOINT = 'https://api.devnet.solana.com';
